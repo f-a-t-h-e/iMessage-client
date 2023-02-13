f98ca4d38835890133a80f5477be0a3664b907de
f-a-t-h-e <77131433+f-a-t-h-e@users.noreply.github.com>
2023-02-13 10:11:06 +0200

    Setup _app.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider } from "@chakra-ui/react";
+import { SessionProvider } from "next-auth/react";
+import type { AppProps } from "next/app";
+import { theme } from "@/chakra/theme";
+import { ApolloProvider } from "@apollo/client";
+import { client as graphQLClient } from "@/graphql/apollo-client";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  console.log("🚀 ~ file: _app.tsx:10 ~ session", session);
+
+  return (
+    <ApolloProvider client={graphQLClient}>
+      <SessionProvider session={session}>
+        <ChakraProvider theme={theme}>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </SessionProvider>
+    </ApolloProvider>
+  );
 }
